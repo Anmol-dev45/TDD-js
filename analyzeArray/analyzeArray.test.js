a9f0a5089a09b5b90ee40a3cbd585b40570c0ed3
Anmol-dev45 <basnetanmol2020@gmail.com>
2024-07-10 09:46:19 +0545

    Created test for empty array

--- analyzeArray/analyzeArray.test.js
+++ analyzeArray/analyzeArray.test.js
@@ -1,5 +1,9 @@
 const analyzeArray = require("./analyzeArray");
 
+test("analyzeArray([]) should return { average: NaN, min: undefined, max: undefined, length: 0 }", () => {
+  expect(analyzeArray([])).toBeNull();
+});
+
 test("analyzeArray([1, 2, 3, 4, 5]) should return { average: 3, min: 1, max: 5, length: 5 }", () => {
   expect(analyzeArray([1, 2, 3, 4, 5])).toEqual({
     average: 3,
